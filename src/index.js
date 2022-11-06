ab13003206699433e946ca63109ab094ab60921f
Andoni-mv <and.moreno18@gmail.com>
2022-11-06 20:41:58 +0100

    port to 8080

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const app = express();
 require('./lib/passport');
 
 // Settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs.engine({
   defaultLayout: 'main',
